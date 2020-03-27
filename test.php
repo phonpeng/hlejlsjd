b545d9607f7aa91061de0809bf5c00b279083656
phonpeng <phonpeng@vip.qq.com>
2020-03-27 17:20:25 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+
+
+    $param = array(
+		"Action"	=>"GetCurrentUserInfo",
+		"SecretId"	=>"AKIDc4Q483nNnMtKcnwtpbq892jusxW8tBYQ",
+    "SecretKey"	=>"kLipgHdjZ9YlacKp7B2g7LL7i5FBuR14",
+		"Timestamp"	=>time(),
+		"Nonce"		=>"34512299",
+		"Signature"	=>"",
+	);
